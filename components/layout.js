60aecef95eeeb751ea43de58fd695e6ee15bbfa5
AdakHaddad <muhammadmuqtada@gmail.com>
2023-06-28 01:17:21 +0700

    Metadata update

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
-import Head from "next/head";
+import React from "react";
+import { Helmet } from "react-helmet";
 
 export const metadata = {
   title: "Bingke 61",
@@ -9,32 +10,38 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <div>
-      <Head>
+      <Helmet>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={metadata.description} />
         <title>{metadata.title}</title>
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Google verification meta tag */}
         <meta
           name="google-site-verification"
           content="jp4c1QT3ewOcc0RQ0_pDKs-I8RlOAciS2CoM2eRREyg"
         />
+
+        {/* Additional meta tags */}
         <meta
           name="keywords"
           content="Indonesian food, Pontianak, Bingke 61, Kalimantan Barat, authentic cuisine"
         />
         <meta name="author" content="Bingke 61" />
         <meta name="robots" content="index, follow" />
+
         {/* Language settings */}
-        <meta http-equiv="content-language" content="id" />
+        <meta httpEquiv="content-language" content="id" />
         <meta name="language" content="Indonesian" />
+
         {/* Open Graph tags for better social media sharing */}
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://bingke61.vercel.app/" />
         <meta property="og:image" content="/public/favicon.jpg" />
-      </Head>
+      </Helmet>
       <main className="flex-grow">{children}</main>
     </div>
   );
