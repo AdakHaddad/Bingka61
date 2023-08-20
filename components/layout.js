4a9306310c1fbfaf5981074cf89415842f8097e7
AdakHaddad <muhammadmuqtada@gmail.com>
2023-08-20 14:59:26 +0700

    Retitle

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 export const metadata = {
   title: "Bingke 61",
   description:
@@ -8,30 +9,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <>
-      <head>
+      <Head>
         <title>{metadata.title}</title>
-        <meta charSet="utf-8" />
+      </Head>
+      <meta charSet="utf-8" />
 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="../public/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={metadata.description} />
+      <link rel="icon" href="../public/favicon.ico" />
 
-        <meta
-          name="keywords"
-          content="Indonesian food, Pontianak, Bingke 61, Kalimantan Barat, authentic cuisine"
-        />
-        <meta name="author" content="Admin" />
-        <meta name="robots" content="index, follow" />
+      <meta
+        name="keywords"
+        content="Indonesian food, Pontianak, Bingke 61, Kalimantan Barat, authentic cuisine"
+      />
+      <meta name="author" content="Admin" />
+      <meta name="robots" content="index, follow" />
 
-        {/* Language settings */}
-        <meta httpEquiv="content-language" content="id" />
-        <meta name="language" content="Indonesian" />
+      {/* Language settings */}
+      <meta httpEquiv="content-language" content="id" />
+      <meta name="language" content="Indonesian" />
 
-        {/* Open Graph tags for better social media sharing */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:type" content="website" />
-      </head>
+      {/* Open Graph tags for better social media sharing */}
+      <meta property="og:title" content={metadata.title} />
+      <meta property="og:description" content={metadata.description} />
+      <meta property="og:type" content="website" />
 
       <main className="flex-grow  bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
         {children}
