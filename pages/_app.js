8bd7b8bdb062f3ed53b24887e58f19b9af123a87
Muhammad Muqtada Alhaddad <94799478+AdakHaddad@users.noreply.github.com>
2025-03-23 18:03:25 +0700

    Update _app.js Analytics on root

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../css/globals.css";
 import RootLayout from "@/components/layout.js";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <RootLayout>
       <Component {...pageProps} />
+      <Analytics /> {/* Ensure Analytics is included */}
     </RootLayout>
   );
 }
