8ba5d9c770bb30809abad4deb8f2996aa4f30c31
AdakHaddad <muhammadmuqtada@gmail.com>
2025-04-02 08:28:36 +0700

    feat: add Webpack configuration to handle fs module dependencies and update package.json with net dependency

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
+  // Webpack configuration should be inside the nextConfig object
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
